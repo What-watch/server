74946c94ea978e27ca55b8e6b9fcec7836991689
tamarazu <tamarazulaika.u@gmail.com>
2020-03-06 09:22:19 +0700

    tixid api fetch data success

--- controllers/tixIdApi.js
+++ controllers/tixIdApi.js
@@ -18,16 +18,14 @@ class TIXIDController{
             }
         })
             .then(response => {
-                console.log('INI RESPONSE +++++++++++++++++++++++++++++++=')
-                console.log(response)
-                res.status(200).json({
-                    data: "ok"
-                })
+                let movies = response.data.results
+                res.status(200).json(movies)
             })
             .catch(err => {
                 console.log(err)
                 res.status(500).json({
-                    error: "Internal Server Error"
+                    error: "Internal Server Error",
+                    err
                 })
             })
     }
@@ -35,16 +33,18 @@ class TIXIDController{
     static movieUpComing(req, res, next){
         axios({
             method: 'GET',
-            url: 'https://api.tix.id/v1/movies/upcoming?',
+            url: 'https://api.tix.id/v1/movies/upcoming',
             headers: {
-                tokenMovie: tokenMovie
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtc2lzZG4iOiIiLCJ1c2VyX2lkIjoiIiwiYXV0aF9zaWduIjoiMmNmNGUxNzktNzQwNS00YWJkLWFlNjAtNTFiMGE2NzhlMzIxIiwicHVycG9zZSI6Im5vdGxvZ2luIiwiYXVkIjoiVGl4SUQgTWlkZGxld2FyZSIsImV4cCI6MTU5MjA5NzYxMCwiaWF0IjoxNTgzNDU3NjEwLCJpc3MiOiJUaXhJRCBTZWN1cml0eSBBdXRob3JpdHkiLCJzdWIiOiJNb2JpbGUgYXV0aG9yaXphdGlvbiB0b2tlbiJ9.q4PiBBerifPNjhxlNO6hL2dgP_nkFzYG7OYxNzBQ8ro"
             },
             params: {
                 city_id : "973818511732248576"
             }
         })
-            .then(movies => {
-                console.log(movies)
+            .then(response => {
+                console.log(response)
+                let movies = response.data.results
+                res.status(200).json(movies)
             })
             .catch(err => {
                 console.log(err)
